5459300664c3608ff428d2448efe25f791828225
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-06 07:00:27 +0000

    created a function to handle input for name

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,6 +3,7 @@ import "../styles/HomePage.css"
 import axios from "axios";
 
 export const HomePage =()=>{
+    const [name, setName] = useState("");
 
 
 
@@ -15,8 +16,14 @@ export const HomePage =()=>{
             </div>
             <div className="PredictAge">
                 <label htmlFor="">Enter Name of the individual</label>
-                <input type="text" />
-               <button className="btn">Predict Age</button>
+                <input 
+                  type="text" 
+                  value={name}
+                  onChange={(event)=>{
+                    setName(event.target.value)
+                  }}
+                />
+               <button className="btn">Predict Age of {name}</button>
                <h3>The predicted age of Name is Age</h3>
             </div>
         </div>
