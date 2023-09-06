ebbb71da4e23ad634579b2c673a46083219c678e
elvisowusu <owusuelvisgyasi@gmail.com>
2023-09-06 06:52:29 +0000

    created to different div to fetch different API's

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,24 @@
 import React,{useState,useEffect} from "react";
 import "../styles/HomePage.css"
+import axios from "axios";
 
 export const HomePage =()=>{
+
+
+
+
     return(
         <div className="main">
-
+            <div className="GenerateCatFacts">
+              <button className="btn">Generate Cat Facts</button>
+              <p></p>
+            </div>
+            <div className="PredictAge">
+                <label htmlFor="">Enter Name of the individual</label>
+                <input type="text" />
+               <button className="btn">Predict Age</button>
+               <h3>The predicted age of Name is Age</h3>
+            </div>
         </div>
     )
 }
